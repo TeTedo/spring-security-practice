49c9607d218e8b76d93b8b6d28de1174b8f257ea
박태석 <qkrxotjr9508@gmail.com>
2023-06-20 15:01:30 +0900

    initial commit

---
+++ src/main/java/com/example/spirngsecuritypractice/SpirngSecurityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirngsecuritypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngSecurityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngSecurityPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spirngsecuritypractice/SpirngSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirngsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
