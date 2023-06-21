b941c911d2699b75a9bda3b151e00f2e89358df3
박태석 <qkrxotjr9508@gmail.com>
2023-06-21 10:26:10 +0900

    테스트용 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,15 @@
-CREATE table `member`
+DROP TABLE IF EXISTS `member_roles`;
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member`
 (
-    id bigint NOT NULL AUTO_INCREMENT,
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username varchar(255) NOT NULL,
     password varchar(255) NOT NULL
-)
+);
+
+CREATE TABLE member_roles (
+    member_id BIGINT,
+    roles VARCHAR(255),
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
